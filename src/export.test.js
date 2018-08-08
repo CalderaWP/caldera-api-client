ddda663c4ad578688823257e7d94bf2af5e86f31
Josh Pollock <josh@calderawp.com>
2018-08-08 14:34:51 -0400

    #11 rm extra white spaces

--- src/export.test.js
+++ src/export.test.js
@@ -9,34 +9,34 @@ import {GeneralSettingClient} from "./GeneralSettingClient";
 
 describe('Export', () => {
 	it('is an object', () => {
-		expect(typeof  clients).toEqual('object')
+		expect(typeof clients).toEqual('object')
 	});
 
 	it('exports factory', () => {
-		expect(typeof  clients.wpClientFactory).toEqual('function')
+		expect(typeof clients.wpClientFactory).toEqual('function')
 	});
 
 	it('exports PrivacySettingsClient', () => {
-		expect(typeof  clients.PrivacySettingsClient).toEqual('function')
+		expect(typeof clients.PrivacySettingsClient).toEqual('function')
 	});
 
 	it('exports ProLocalSettingClient', () => {
-		expect(typeof  clients.ProLocalSettingClient).toEqual('function')
+		expect(typeof clients.ProLocalSettingClient).toEqual('function')
 	});
 	it('exports WpClient', () => {
-		expect(typeof  clients.WpClient).toEqual('function')
+		expect(typeof clients.WpClient).toEqual('function')
 	});
 	it('exports FormsClient', () => {
-		expect(typeof  clients.FormsClient).toEqual('function')
+		expect(typeof clients.FormsClient).toEqual('function')
 	});
 	it('exports EntriesClient', () => {
-		expect(typeof  clients.EntriesClient).toEqual('function')
+		expect(typeof clients.EntriesClient).toEqual('function')
 	});
 	it('exports ProClient', () => {
-		expect(typeof  clients.ProClient).toEqual('function')
+		expect(typeof clients.ProClient).toEqual('function')
 	});
 	it('exports GeneralSettingClient', () => {
-		expect(typeof  clients.GeneralSettingClient).toEqual('function')
+		expect(typeof clients.GeneralSettingClient).toEqual('function')
 	});
 
 });
