1ccd5d219894cd392bbbab2842479a4d91eb6d35
Josh Pollock <josh@calderawp.com>
2018-08-08 14:32:29 -0400

    export new clients #11

---
+++ src/export.test.js
@@ -0,0 +1,42 @@
+import clients from './index';
+import {PrivacySettingsClient} from "./PrivacySettingsClient";
+import {ProLocalSettingClient} from "./ProLocalSettingClient";
+import {WpClient} from "./WpClient";
+import {FormsClient} from "./FormsClient";
+import {EntriesClient} from "./EntriesClient";
+import {ProClient} from "./ProClient";
+import {GeneralSettingClient} from "./GeneralSettingClient";
+
+describe('Export', () => {
+	it('is an object', () => {
+		expect(typeof  clients).toEqual('object')
+	});
+
+	it('exports factory', () => {
+		expect(typeof  clients.wpClientFactory).toEqual('function')
+	});
+
+	it('exports PrivacySettingsClient', () => {
+		expect(typeof  clients.PrivacySettingsClient).toEqual('function')
+	});
+
+	it('exports ProLocalSettingClient', () => {
+		expect(typeof  clients.ProLocalSettingClient).toEqual('function')
+	});
+	it('exports WpClient', () => {
+		expect(typeof  clients.WpClient).toEqual('function')
+	});
+	it('exports FormsClient', () => {
+		expect(typeof  clients.FormsClient).toEqual('function')
+	});
+	it('exports EntriesClient', () => {
+		expect(typeof  clients.EntriesClient).toEqual('function')
+	});
+	it('exports ProClient', () => {
+		expect(typeof  clients.ProClient).toEqual('function')
+	});
+	it('exports GeneralSettingClient', () => {
+		expect(typeof  clients.GeneralSettingClient).toEqual('function')
+	});
+
+});
--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import {PrivacySettingsClient} from "./PrivacySettingsClient";
 import {WpClient} from "./WpClient";
 import {FormsClient} from "./FormsClient";
 import {EntriesClient} from "./EntriesClient";
+import {ProClient} from "./ProClient";
+import {GeneralSettingClient} from "./GeneralSettingClient";
 
+/**
+ * Export factory and all clients
+ * @type {{wpClientFactory: wpClientFactory, PrivacySettingsClient: PrivacySettingsClient, ProLocalSettingClient: ProLocalSettingClient, WpClient: WpClient, FormsClient: FormsClient, EntriesClient: EntriesClient, ProClient: ProClient, GeneralSettingClient: GeneralSettingClient}}
+ */
 module.exports = {
 	wpClientFactory,
 	PrivacySettingsClient,
 	ProLocalSettingClient,
 	WpClient,
 	FormsClient,
-	EntriesClient
+	EntriesClient,
+	ProClient,
+	GeneralSettingClient
 };
